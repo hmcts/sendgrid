e58b5168b64ec95743529221c5fef95dfc2c675a
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-03-12 11:04:06 +0000

    updated variable types

--- terraform/01-inputs-required.tf
+++ terraform/01-inputs-required.tf
@@ -1,13 +1,7 @@
 variable "environment" {
   description = "SendGrid Configuration"
-  type        = string
 }
 
 variable "accounts" {
   description = "SendGrid subuser accounts"
-  type   = list(object({
-    name = string
-    ips  = list(string)
-    })
-  )
 }
