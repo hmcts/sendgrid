97fa346dc7afb9e47a67647441359507a60db39f
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-02-16 11:15:23 +0000

    updated init for prod and nonprod

--- terraform/nonprod/00-init.tf
+++ terraform/nonprod/00-init.tf
@@ -1,5 +1,24 @@
 terraform {
   required_version = ">= 1.2.2"
-  backend "azurerm" {}
+  # backend "azurerm" {}
+  required_providers {
+    azurerm = "~> 3.11.0"
+    sendgrid = {
+      source  = "anna-money/sendgrid"
+      version = "1.0.4"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  alias = "api_key_vault"
+  features {}
+}
+
+provider "sendgrid" {
+  features {}
+}
