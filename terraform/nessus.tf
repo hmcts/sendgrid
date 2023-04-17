82a6fc26ddb8959389cca4c90f87fa9fbc1bd963
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-04-17 11:03:07 +0100

    Setup nessus account (#37)

---
+++ terraform/nessus.tf
@@ -0,0 +1,17 @@
+locals {
+  nessus_non_prod_domains = ["mail-nessus-nonprod.platform.hmcts.net"]
+  nessus_prod_domains     = ["mail-nessus.platform.hmcts.net"]
+}
+
+module "nessus" {
+  source      = "./modules/sendgrid"
+  environment = var.environment
+  account     = "nessus"
+  domains     = var.environment == "prod" ? local.nessus_prod_domains : local.nessus_non_prod_domains
+}
+
+module "nessus-dns" {
+  source      = "./modules/azure_dns"
+  dns_records = module.nessus.dns_records
+  zone_name   = "platform.hmcts.net"
+}
