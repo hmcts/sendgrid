e6fb647524a149452e13b27aa1eb792fe05d1eed
Tim Jacomb <timjacomb1@gmail.com>
2023-04-20 10:08:45 +0100

    Typo

--- terraform/pcol.tf
+++ terraform/pcol.tf
@@ -5,7 +5,7 @@ module "pcol" {
   # pcol uses noreply-pcol@hmcts.net for both NLE and prod
   # 'difficult' to get it to change like the rest of the services that each use a unique domain
   domains              = ["hmcts.net"]
-  custom_dkim_selector = var.environment == prod ? "prd" : "pnl"
+  custom_dkim_selector = var.environment == "prod" ? "prd" : "pnl"
 }
 
 module "pcol-dns" {
