942fd12b76703a97aa18fbba2329a7bd209e551c
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-02-20 09:23:45 +0000

    add utiac.tf back in, minus prod domain

---
+++ components/sendgrid/utiac.tf
@@ -0,0 +1,17 @@
+locals {
+  utiac_non_prod_domains = ["mail-utiac-nonprod.platform.hmcts.net"]
+  utiac_prod_domains     = [""]
+}
+
+module "utiac" {
+  source      = "../modules/sendgrid"
+  environment = var.env
+  account     = "utiac"
+  domains     = var.env == "prod" ? local.utiac_prod_domains : local.utiac_non_prod_domains
+}
+
+module "utiac-dns" {
+  source      = "../modules/azure_dns"
+  dns_records = module.utiac.dns_records
+  zone_name   = "platform.hmcts.net"
+}
