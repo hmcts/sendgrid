1ca6c7ac7964c7922bb49a65ca52c9e58979db36
Marty Fox <martin.fox1@justice.gov.uk>
2025-06-17 11:47:11 +0100

    Adding BAU specific sendgrid account for all BAU requirements that might arise

--- components/sendgrid/bau.tf
+++ components/sendgrid/bau.tf
@@ -1,15 +1,15 @@
 # This config is designated for BAU/Platform Ops use only.
 
 locals {
-  panorama_non_prod_domains = ["mail-bau-nonprod.platform.hmcts.net"]
-  panorama_prod_domains     = ["mail-bau.platform.hmcts.net"]
+  bau_non_prod_domains = ["mail-bau-nonprod.platform.hmcts.net"]
+  bau_prod_domains     = ["mail-bau.platform.hmcts.net"]
 }
 
 module "bau" {
   source      = "../modules/sendgrid"
   environment = var.env
   account     = "bau"
-  domains     = var.env == "prod" ? local.panorama_prod_domains : local.panorama_non_prod_domains
+  domains     = var.env == "prod" ? local.bau_prod_domains : local.bau_non_prod_domains
 }
 
 module "bau-dns" {
