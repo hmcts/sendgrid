6aed458616f95717ad551006950439c81592cbd7
ashleighB31 <Ashleigh.Brennan@justice.gov.uk>
2026-04-21 14:42:22 +0100

    removing pcol chnages

--- components/sendgrid/dcs.tf
+++ components/sendgrid/dcs.tf
@@ -1,25 +1,25 @@
-# locals {
-#   dcs_non_prod_domains = ["mail-dcs-nonprod.platform.hmcts.net", "hmcts.net"]
-#   dcs_prod_domains     = ["mail-dcs.platform.hmcts.net", "hmcts.net"]
-# }
+locals {
+  dcs_non_prod_domains = ["mail-dcs-nonprod.platform.hmcts.net", "hmcts.net"]
+  dcs_prod_domains     = ["mail-dcs.platform.hmcts.net", "hmcts.net"]
+}
 
-# module "dcs" {
-#   source      = "../modules/sendgrid"
-#   environment = var.env
-#   account     = "dcs"
-#   domains     = var.env == "prod" ? local.dcs_prod_domains : local.dcs_non_prod_domains
-#   custom_dkim_selector = var.env == "prod" ? "dcs" : "dcn"
-# }
+module "dcs" {
+  source      = "../modules/sendgrid"
+  environment = var.env
+  account     = "dcs"
+  domains     = var.env == "prod" ? local.dcs_prod_domains : local.dcs_non_prod_domains
+  custom_dkim_selector = var.env == "prod" ? "dcs" : "dcn"
+}
 
-# module "dcs-dns" {
-#   source      = "../modules/azure_dns"
-#   dns_records = { for k, v in module.dcs.dns_records : k => v if k != "hmcts.net" }
-#   zone_name   = "platform.hmcts.net"
-# }
+module "dcs-dns" {
+  source      = "../modules/azure_dns"
+  dns_records = { for k, v in module.dcs.dns_records : k => v if k != "hmcts.net" }
+  zone_name   = "platform.hmcts.net"
+}
 
-# module "dcs-dns-hmcts" {
-#   source      = "../modules/azure_dns"
-#   dns_records = { for k, v in module.dcs.dns_records : k => v if k == "hmcts.net" }
-#   zone_name   = "hmcts.net"
-# }
+module "dcs-dns-hmcts" {
+  source      = "../modules/azure_dns"
+  dns_records = { for k, v in module.dcs.dns_records : k => v if k == "hmcts.net" }
+  zone_name   = "hmcts.net"
+}
 
